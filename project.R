4c40c4f081e8fcd0ff2ddce97aeb744c7280ee67
Sean Konovitz <seankono@buffalo.edu>
2018-11-13 17:16:58 -0500

    changes

--- project.R
+++ project.R
@@ -1,52 +1,28 @@
-install.packages("RSocrata")
-install.packages("maptools")
-install.packages("tidycensus")
-install.packages("dplyr")
-install.packages("GISTools")
-
-library("RSocrata")
-
-crime <- read.socrata(
-  "https://data.buffalony.gov/resource/d6g9-xbgu.json",
-  app_token = "y67OCQdGIHD80tqceGSZV8rj1",
-  email     = "seankono@buffalo.edu",
-  password  = "Skbuff66$")
-View(crime)
-
-
-bufftract <- read.socrata(
-  "https://data.buffalony.gov/resource/pg8k-g5iz.json",
-  app_token = "y67OCQdGIHD80tqceGSZV8rj1",
-  email     = "seankono@buffalo.edu",
-  password  = "Skbuff66$")
-View(bufftract)
-
-
-
-
 library(tidycensus)
 library(tidyverse)
-census_api_key("a228276714dbd52068673939d5ccb03af28a4cab", overwrite = FALSE, install = TRUE)
-Sys.getenv("a228276714dbd52068673939d5ccb03af28a4cab")
 
 erie<- get_acs(state = "NY", county = "Erie", geography = "tract", 
                   variables = "B19013_001", geometry = TRUE)
-head(erie)
-View(erie)
+load("data/eriecensus.Rda")
+
+
+
+library(sf)
+
+buffalotracts<- read_sf("data/geo_export_c9004500-af2b-4c4d-8cde-4de9d5831f2e.shp")
+View(buffalotracts)       
+load("data/bufftract.Rda")
+
+library(sf)
+crimepoints<- st_as_sf(crime, coords=c("longitude", "latitude"),
+                         crs= 4326)
+load("data/crimepoints.Rda")
 
 library(dplyr)
-
 joined.tracts <- merge(bufftract, erie, by.x = "geoid10", by.y = "GEOID",
-                       all.y = FALSE)
-View(joined.tracts)
+                       +all.y = FALSE)
+View(crimepoints)
 
-library(GISTools)
-library(sp)
+plot(crimepoints)
 
-library(maptools)
-buffalotracts<- readShapePoly("C:/Users/Asus/Documents/R/R(Git)/geo503-2018-seankono/geo_export_c9004500-af2b-4c4d-8cde-4de9d5831f2e.shp")
 
-buffalotracts<- readShapePoly("C:/Users/Asus/Documents/R/R(Git)/geo503-2018-seankono/geo_export_c9004500-af2b-4c4d-8cde-4de9d5831f2e.shp")
-
-?SpatialPointsDataFrame
-crimepoints<- SpatialPointsDataFrame(crime, data=)
